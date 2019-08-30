cdc035a05df059f4509b703f87a7080df176879e
github username <github email address>
2019-08-30 16:48:31 -0400

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre
+    attr_accessor :name, :artist
+    @@all = []
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all
+    end
+
+    def artists
+        Artist.all
+    end
+
+    def self.all
+        @@all 
+    end
+
+end
